f648131e3fc811ad56ae64320c1780cfeebeddb0
Petar Kolev <petar.kolev@ocado.com>
2023-12-17 00:57:57 +0200

    renaming

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 var bodyParser = require('body-parser');
-const authRouter = require("./auth/router");
-const userRouter = require("./user/router");
-const clubRouter = require("./club/router");
+const authRouter = require("./auth/AuthRouter");
+const userRouter = require("./user/UserRouter");
+const clubRouter = require("./club/ClubRouter");
 
 const app = express();
 app.use(bodyParser.json())
