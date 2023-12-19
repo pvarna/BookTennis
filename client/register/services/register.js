94d03d16bbca7bde968b60f892b7fe579120a124
Sonya Nikolova <snikolova27@gmail.com>
2023-12-19 19:46:25 +0200

    address comments

--- client/register/services/register.js
+++ client/register/services/register.js
@@ -1,48 +1,41 @@
 const validatePhoneNumber = (phone) => {
-    const isNum = /^\d+$/.test(phone);
+  const isNum = /^\d+$/.test(phone);
 
-    return isNum && phone.length === 10
-}
+  return isNum && phone.length === 10;
+};
 
 const onRegisterHandler = () => {
-    const email = document.getElementById("email").value
-    console.log("email: ", email)
+  const email = document.getElementById("register-email").value;
+  console.log("email: ", email);
 
-    if(email.length === 0){
-        window.alert("Email cannot be empty!")
-    }
+  if (email.length === 0) {
+    window.alert("Email cannot be empty!");
+  }
 
-    const fullname = document.getElementById("name").value
-    console.log("fullname: ", fullname)
+  const fullname = document.getElementById("register-name").value;
+  console.log("fullname: ", fullname);
 
-    if(fullname.length === 0){
-        window.alert("Fullname cannot be empty!")
-    }
+  if (fullname.length === 0) {
+    window.alert("Fullname cannot be empty!");
+  }
 
-    const pass = document.getElementById("pass").value
-    console.log("pass: ", pass)
-    
-    
-    if(pass.length === 0){
-        window.alert("Password cannot be empty!")
-    }
+  const pass = document.getElementById("register-pass").value;
+  console.log("pass: ", pass);
 
-    const city = document.getElementById("city").value
-    console.log("city: ", city)
+  if (pass.length === 0) {
+    window.alert("Password cannot be empty!");
+  }
 
-    const phone = document.getElementById("phone").value
-    console.log("phone: ", phone)
+  const city = document.getElementById("register-city").value;
+  console.log("city: ", city);
 
-    
-    // Validate phone is 10 digits long
-    const isPhoneNumberValid = validatePhoneNumber(phone)
-    if(phone.length === 0){
-        window.alert("Phone number cannot be empty!")
-    }
-    if(!isPhoneNumberValid && phone.length > 0){
-        window.alert("Phone number should contain only 10 digits.")
-        phone.value=""
-    }
-    }
-    
-    
+  const phone = document.getElementById("register-phone").value;
+  console.log("phone: ", phone);
+
+  // Validate phone is 10 digits long
+  const isPhoneNumberValid = validatePhoneNumber(phone);
+  if (!isPhoneNumberValid) {
+    window.alert("Phone number should contain only 10 digits.");
+    phone.value = "";
+  }
+};
